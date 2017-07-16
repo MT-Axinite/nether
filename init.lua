abac39b2359680c4d507247ee1f62a2f01e07644
Axinite <Admin Axinite // taikedz.net>
2017-07-16 18:11:27 +0000

    Initial Axinite commit

---
+++ init.lua
@@ -0,0 +1,17 @@
+-- Another Nether Mod
+--Tweaked by paramat and maikerumine
+
+local path = minetest.get_modpath("nether")
+
+
+dofile(path .. "/nodes.lua") -- Node definition
+dofile(path .. "/quartz.lua") -- Quartz definition
+dofile(path .. "/crafts.lua") -- Crafting
+dofile(path .. "/mapgen.lua") -- Map generation and Biome definitions
+dofile(path .. "/nether_mines.lua") -- Mines based off of tsm_mines
+
+dofile(path .. "/portal.lua") -- Portal Code
+
+
+
+
